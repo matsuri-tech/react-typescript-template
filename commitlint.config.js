b1e497dd69fa2d791e13e4e62fd25c70b9717aa9
hrdtbs <harada.tsubasa.0422@gmail.com>
2019-12-05 18:08:02 +0900

    chore(commitlint): disable subject-case rule and scope-case rule

--- commitlint.config.js
+++ commitlint.config.js
@@ -1,1 +1,8 @@
-module.exports = { extends: ["@commitlint/config-conventional"] }
+module.exports = {
+    extends: ["@commitlint/config-conventional"],
+    rules: {
+        //@see https://commitlint.js.org/#/reference-rules
+        "subject-case": [0],
+        "scope-case": [0]
+    }
+}
