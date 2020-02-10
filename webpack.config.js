b5d99916e795e4876532a55f2564541118bdfb4c
hrdtbs <harada.tsubasa.0422@gmail.com>
2020-02-10 15:38:59 +0900

    chore(scripts): add analyze script

--- webpack.config.js
+++ webpack.config.js
@@ -8,6 +8,7 @@ const MiniCssExtractPlugin = require("mini-css-extract-plugin")
 const dotenv = require("dotenv")
 const pkg = require("./package.json")
 const TerserPlugin = require("terser-webpack-plugin")
+const StatsPlugin = require("stats-webpack-plugin")
 
 module.exports = (_, { mode = "development" }) => {
     const env = dotenv.config().parsed
@@ -39,6 +40,9 @@ module.exports = (_, { mode = "development" }) => {
         },
         /** @see https://webpack.js.org/configuration/devtool/#devtool */
         devtool: "",
+        resolve: {
+            extensions: [".js", ".jsx", ".ts", ".tsx"]
+        },
         module: {
             rules: [
                 {
@@ -46,7 +50,10 @@ module.exports = (_, { mode = "development" }) => {
 
                     exclude: /node_modules/,
                     use: {
-                        loader: "babel-loader"
+                        loader: "babel-loader",
+                        options: {
+                            babelrc: true
+                        }
                     }
                 },
                 {
@@ -160,7 +167,11 @@ module.exports = (_, { mode = "development" }) => {
                     `"${process.env.NODE_ENV}"` || '"development"',
                 ...envKeys
             }),
-            new webpack.HotModuleReplacementPlugin()
+            new webpack.HotModuleReplacementPlugin(),
+            new StatsPlugin("stats.json", {
+                chunkModules: true,
+                exclude: [/node_modules[\\/]react/]
+            })
         ]
         config.devServer = {
             historyApiFallback: true,
